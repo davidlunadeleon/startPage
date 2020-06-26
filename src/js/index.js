6dee3c8a78295971ace21043b1970eb69d8e5679
davidlunadeleon <davidlunadeleon@gmail.com>
2020-06-26 00:09:09 -0500

    Add list of saved sites - Load list of saved sites from json file - Make list of sections with links in each one - Style the tree of links - Add module to handle tree creation

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import '../css/style.css';
 import  * as config from '../config/config.json';
 import backgroundImage from '../assets/background.jpg'
+import { makeTree } from './tree.js';
 
 //Source of the ready function: http://youmightnotneedjquery.com/
 
@@ -17,4 +18,5 @@ ready(() => {
     document.getElementById('searchForm').action = config.searchEngine;
     document.getElementById('searchInput').placeholder = config.placeHolderMessage;
     document.getElementById('backgroundImage').src = backgroundImage;
+    makeTree();
 })
---
+++ src/js/tree.js
@@ -0,0 +1,25 @@
+import * as config from '../config/config.json';
+
+function makeTree() {
+    config.sitesTree.forEach(section => {
+        var htmlSection = document.createElement('li');
+        var headerSection = document.createElement('h1');
+        headerSection.textContent = section.sectionName;
+        htmlSection.appendChild(headerSection);
+        var sectionList = document.createElement('ul');
+        section.sites.forEach(site => {
+            var siteItem = document.createElement('li');
+            var siteHeader = document.createElement('h2');
+            var link = document.createElement('a');
+            link.href = site.url;
+            link.textContent = site.siteName;
+            siteHeader.appendChild(link);
+            siteItem.appendChild(siteHeader);
+            sectionList.appendChild(siteItem);
+        });
+        document.getElementById('tree').appendChild(htmlSection);
+        htmlSection.parentNode.insertBefore(sectionList, htmlSection.nextSibling);
+    });
+}
+
+export { makeTree };
