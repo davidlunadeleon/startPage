989848b331fd322e1059272c3f60e561ec59e16b
davidlunadeleon <davidlunadeleon@gmail.com>
2020-06-26 17:04:31 -0500

    Add greeting message by time of the day  - Add greeting message that changes with time

---
+++ src/js/greetings.js
@@ -0,0 +1,20 @@
+import * as config from '../config/config.json';
+
+function sortAux(a, b){
+    return a.until - b.until;
+}
+
+function greetings() {
+    var time = new Date();
+    var hours = time.getHours();
+    var minutes = time.getMinutes();
+    var greetingsMap = config.greetingMessages;
+    greetingsMap.sort(sortAux);
+    var i = 0;
+    while(hours * 100 + minutes > greetingsMap[i].until){
+        i++;
+    }
+    document.getElementById('greetingMessage').innerHTML = greetingsMap[i].message + config.name;
+}
+
+export { greetings };
--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@ import '../css/style.css';
 import  * as config from '../config/config.json';
 import backgroundImage from '../assets/background.jpg'
 import { makeTree } from './tree.js';
+import { greetings } from './greetings.js';
 
 //Source of the ready function: http://youmightnotneedjquery.com/
 
@@ -14,9 +15,13 @@ function ready(cb) {
 }
 
 ready(() => {
-    document.title = config.name;
+    document.title = config.pageTitle;
     document.getElementById('searchForm').action = config.searchEngine;
     document.getElementById('searchInput').placeholder = config.placeHolderMessage;
     document.getElementById('backgroundImage').src = backgroundImage;
     makeTree();
+    greetings();
+    document.body.setInterval(() => {
+       greetings(); 
+    }, 60000);
 })
