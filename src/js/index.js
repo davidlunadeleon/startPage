a67184b694ed5633c7b508b341716054eeaf26a3
davidlunadeleon <davidlunadeleon@gmail.com>
2020-06-25 22:44:51 -0500

    Fix white border around background image  - Load image from javascript  - Change html to have image tag  - Add css styles to fix error

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import '../css/style.css';
 import  * as config from '../config/config.json';
+import backgroundImage from '../assets/background.jpg'
 
 //Source of the ready function: http://youmightnotneedjquery.com/
 
@@ -15,4 +16,5 @@ ready(() => {
     document.title = config.name;
     document.getElementById('searchForm').action = config.searchEngine;
     document.getElementById('searchInput').placeholder = config.placeHolderMessage;
+    document.getElementById('backgroundImage').src = backgroundImage;
 })
