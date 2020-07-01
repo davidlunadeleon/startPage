d23cdf7487e595ace0784846cdef072aa64cf21e
davidlunadeleon <davidlunadeleon@gmail.com>
2020-06-30 21:00:39 -0500

    Create extension preferences page  - Load options template from webpack  - Add options ui to extension manifest  - Create placeholder files for the options

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: {
-        index: './src/js/index.js'
+        index: './src/js/index.js',
+        options: './src/js/options.js'
     },
     output: {
         path: path.resolve(__dirname, 'dist/web'),
@@ -13,9 +14,17 @@ module.exports = {
     plugins: [
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
+            chunks: ['index'],
+            filename: 'index.html',
             cache: false,
             template: './src/html/index.html'
-        })
+        }),
+        new HtmlWebpackPlugin({
+            chunks: ['options'],
+            filename: 'options.html',
+            cache: false,
+            template: './src/html/options.html'
+        }),
     ],
     module: {
         rules: [
